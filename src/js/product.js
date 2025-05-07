4f3a39da935b05e73267b51543f0dea7dee8adb5
Arthur Weale <ajayrossy340@gmail.com>
2025-05-08 01:19:50 +0200

    Format code

--- src/js/product.js
+++ src/js/product.js
@@ -2,14 +2,29 @@ import { setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
+const cartItemsObject = {};
 
 function addProductToCart(product) {
+  if(localStorage.getItem("so-cart")){
+    const parsedCart = JSON.parse(localStorage.getItem("so-cart") || {});
+    Object.assign(cartItemsObject, parsedCart)
+  }
   setLocalStorage("so-cart", product);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
+  const newId = product.Id;
+  
+  //Here is my thought process
+  /**********************************************
+ * In this function the product is being passed to addProductToCart as an argument
+ * Instead i think need to pass in an object instead
+ **********************************************/
+
+  cartItemsObject[newId]= product;
+  console.log(JSON.stringify(cartItemsObject,null,5))
+  addProductToCart(cartItemsObject);
 }
 
 // add listener to Add to Cart button
