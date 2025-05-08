69b0cf7c50870eae5cc0d928280163d2c694e2b9
Arthur Weale <ajayrossy340@gmail.com>
2025-05-08 03:29:50 +0200

    Test: Cart page is rendering a product

--- src/js/product.js
+++ src/js/product.js
@@ -2,29 +2,32 @@ import { setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
-const cartItemsObject = {};
+const cartItemsArray = [];
 
 function addProductToCart(product) {
-  if(localStorage.getItem("so-cart")){
-    const parsedCart = JSON.parse(localStorage.getItem("so-cart") || {});
-    Object.assign(cartItemsObject, parsedCart) //Assign to object.
+  
+  if(!localStorage.getItem("so-cart")){
+    
+    cartItemsArray.push(product);
+    //const products = JSON.stringify(cartItemsArray)
+  }else{
+    cartItemsArray.push(product);
   }
-  setLocalStorage("so-cart", product);
+  
+  //console.log(cartItemsArray);
+  setLocalStorage("so-cart", cartItemsArray);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
-  const newId = product.Id;
-  
+  console.log(JSON.stringify(product, null, 3))
   //Here is my thought process
   /**********************************************
  * In this function the product is being passed to addProductToCart as an argument
- * Instead i think need to pass in an object instead
+ * Instead i think need to pass in an array of elements instead
  **********************************************/
 
-  cartItemsObject[newId]= product;
-  console.log(JSON.stringify(cartItemsObject,null,5))
-  addProductToCart(cartItemsObject);
+  addProductToCart(product);
 }
 
 // add listener to Add to Cart button
