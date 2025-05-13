34fa1b86f18b292ed3b7b7fa7283d3a52045b871
FrodoPano <frodopano@gmail.com>
2025-05-13 15:20:04 -0600

    update

--- src/js/product.js
+++ src/js/product.js
@@ -1,17 +1,19 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+import { setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  const cartItems = getLocalStorage("so-cart") || []; // get cart array of items from local storage if null set to empty array
-  cartItems.push(product);
-  setLocalStorage("so-cart", cartItems);
+  setLocalStorage("so-cart", product);
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
   addProductToCart(product);
+  
+  // Optional: Show a notification that the item was added to cart
+  alert("Item added to cart!");
 }
 
 // add listener to Add to Cart button
