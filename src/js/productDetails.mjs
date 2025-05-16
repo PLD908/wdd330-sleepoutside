e0d86549e9e880857fa4d2b934d03d3d6f32f7a6
Arthur Weale <ajayrossy340@gmail.com>
2025-05-16 03:18:50 +0200

    Added file

---
+++ src/js/productDetails.mjs
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+export default class productDetails{ //Tracks and establishes the product
+    constructor(){ //This initialises the object providing the build template
+        this.category = category;
+        this.path = `../json/${this.category}.json`;
+        this.product = {};
+    }
+
+    init(){
+
+    }
+}
