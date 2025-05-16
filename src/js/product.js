b58e1cc2a0d205a2aced1b346d17b5e99ab1e9d4
Arthur Weale <ajayrossy340@gmail.com>
2025-05-16 03:21:13 +0200

    Removed addProductToCart function

--- src/js/product.js
+++ src/js/product.js
@@ -3,9 +3,7 @@ import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
-function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
-}
+
 
 // add to cart button event handler
 async function addToCartHandler(e) {
